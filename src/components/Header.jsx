f8040e9980e7097baa28627099be513a229bcac4
theodor164 <tanase.ted.164@gmail.com>
2023-11-26 01:40:23 +0200

    added routes for home and for account creation

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import styles from '../styles/Header.module.scss';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>Horiab</h1>
+      {/* Add navigation links or user authentication status */}
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/AccountCreation.jsx
@@ -0,0 +1,12 @@
+
+const AccountCreation = () => {
+  // Implement the account creation form
+  return (
+    <div>
+      <h2>Create an Account</h2>
+      {/* Add form components */}
+    </div>
+  );
+};
+
+export default AccountCreation;
