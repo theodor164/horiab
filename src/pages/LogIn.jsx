88c52892af527530f8fbcb57a96b6a3284785378
theodor164 <tanase.ted.164@gmail.com>
2023-11-27 03:43:23 +0200

    added the code for the login page

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,12 +1,53 @@
 import Header from "../components/Header";
+import { useState } from 'react';
+import axios from 'axios';
 
-const LogIn = () => {
-  // Implement the account creation form
+const LogIn = ({ history }) => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:3001/api/login', formData);
+
+      console.log(response.data); // Handle the response as needed
+
+      // For simplicity, let's redirect to a dashboard page on successful login
+      history.push('/dashboard');
+    } catch (error) {
+      console.error('Login error:', error.response.data.error);
+      // Handle the error and show a message to the user
+    }
+  };
+
   return (
     <div>
       <Header></Header>
-      <h2>Log In</h2>
-      {/* Add form components */}
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" name="username" value={formData.username} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" name="password" value={formData.password} onChange={handleChange} />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 };
