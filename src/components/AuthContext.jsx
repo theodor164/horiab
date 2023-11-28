59e759eddd78e89b2c8561ba4f11fad4266e5c87
theodor164 <tanase.ted.164@gmail.com>
2023-11-28 19:17:13 +0200

    the subscribe button now is displayed based on whether the user is subscribed or not

--- src/components/AuthContext.jsx
+++ src/components/AuthContext.jsx
@@ -1,5 +1,6 @@
 // AuthContext.js
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
+import axios from 'axios'; // Import axios
 
 const AuthContext = createContext();
 
@@ -18,14 +19,38 @@ export const AuthProvider = ({ children }) => {
     setUsername(''); // Reset the username when the user logs out
   };
 
+  const subscribe = () => {
+    // Set isSubscribed to true when the user subscribes
+    setIsSubscribed(true);
+  };
+
+  const unsubscribe = () => {
+    // Set isSubscribed to false when the user unsubscribes
+    setIsSubscribed(false);
+  };
+
+  useEffect(() => {
+    // Fetch user data on authentication to get subscription status
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3001/api/userdata?username=${username}`);
+        setIsSubscribed(response.data.subscription.status);
+      } catch (error) {
+        console.error('Error fetching user data:', error.response ? error.response.data : error.message);
+      }
+    };
+
+    if (isAuthenticated && username) {
+      fetchUserData();
+    }
+  }, [isAuthenticated, username]);
   return (
-    <AuthContext.Provider value={{ isAuthenticated, username, login, logout }}>
+    <AuthContext.Provider value={{ isAuthenticated, username, isSubscribed, login, logout, subscribe, unsubscribe }}>
       {children}
     </AuthContext.Provider>
   );
 };
 
-console.log('hello');
 export const useAuth = () => {
   return useContext(AuthContext);
 };
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -6,26 +6,25 @@ import UserData from '../components/UserData';
 import { useAuth } from '../components/AuthContext';
 
 const DashboardPage = () => {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, isSubscribed } = useAuth();
   if (!isAuthenticated) {
     // Redirect to the login page if not authenticated
     return <Navigate to="/login" />;
   }
+  console.log(isSubscribed);
   return (
     <div>
       <Header></Header>
       <h1>Dashboard Page</h1>
       {/* Add content for the dashboard */}
       <UserData></UserData>
-
-       {/* Add the Stripe buy button component */}
-       
+    {/* Conditionally render the Stripe buy button if the user is not subscribed */}
+    {!isSubscribed && (
         <stripe-buy-button
           buy-button-id="buy_btn_1OHT85JtqR3ifyMox1BwbVSq"
           publishable-key="pk_live_51OHSZJJtqR3ifyMoxZjB2SLvNxAIu8LYfH0eU3mi49U0kI44qkfCLJNlQshyK0OT8KhwvEo36jpXwxp2ayBjxQhG00rENiAOEZ">
         </stripe-buy-button>
-      
-      
+      )}
     </div>
   );
 };
