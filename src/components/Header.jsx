ff49f3ce3126f12aa5e4e8987e8569b492281ddf
theodor164 <tanase.ted.164@gmail.com>
2023-11-28 15:52:57 +0200

    added the logout function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,25 @@
 import styles from '../styles/Header.module.scss';
 import { Link } from 'react-router-dom';
+import { useAuth } from './AuthContext';
 
 const Header = () => {
+  const { isAuthenticated, logout } = useAuth();
+
+  const handleLogout = () => {
+    logout();
+  };
+
   return (
     <header className={styles.header}>
       <h1>Horiab</h1>
       {/* Add navigation links or user authentication status */}
       <nav>
         <Link to="/">Home </Link>
-        <Link to="/accountCreationPage">Create Account </Link>
+        {!isAuthenticated &&  <Link to="/accountCreationPage">Create Account </Link>}
         <Link to="/logInPage">Account </Link>
         <Link to="/videosPage">Videos </Link>
         <Link to="/dashboard">Dashboard </Link>
+        {isAuthenticated && <button onClick={handleLogout}>Logout</button>}
       </nav>
     </header>
   );
